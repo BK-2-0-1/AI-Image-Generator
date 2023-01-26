b80d1c64fea5fc8a4bba694bec87b0fce418ec6b
BK-2-0-1 <68499416+BK-2-0-1@users.noreply.github.com>
2023-01-26 12:35:20 +0300

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -30,7 +30,7 @@ const CreatePost = () => {
       try {
         setGeneratingImg(true);
         const response = await fetch(
-          'https://ai-image-generator-d0es.onrender.com/api/v1/dalle',
+          'https://dalle-arbb.onrender.com/api/v1/dalle',
           {
             method: 'POST',
             headers: {
@@ -61,7 +61,7 @@ const CreatePost = () => {
       setLoading(true);
       try {
         const response = await fetch(
-          'https://ai-image-generator-d0es.onrender.com/api/v1/post',
+          'https://dalle-arbb.onrender.com/api/v1/post',
           {
             method: 'POST',
             headers: {
