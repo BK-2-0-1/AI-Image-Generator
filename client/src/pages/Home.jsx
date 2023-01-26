432cf875deaeeb4bee1fd15b0e57014c7da0c32e
BK-2-0-1 <68499416+BK-2-0-1@users.noreply.github.com>
2023-01-26 12:29:42 +0300

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
 
     try {
       const response = await fetch(
-        'https://dalle-arbb.onrender.com/api/v1/post',
+        'https://ai-image-generator-d0es.onrender.com/api/v1/post',
         {
           method: 'GET',
           headers: {
